1957fa941505d2b5095240aea3e83bd3276bd1fd
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-07-18 23:17:16 +0400

    - Added a staging custom env

--- projects.tf
+++ projects.tf
@@ -8,3 +8,26 @@ resource "vercel_project" "portfolio" {
     repo = "mervinhemaraju/portfolio"
   }
 }
+
+
+resource "vercel_custom_environment" "staging" {
+  project_id  = vercel_project.portfolio.id
+  name        = "staging"
+  description = "A description of the custom environment"
+  branch_tracking = {
+    pattern = "staging"
+    type    = "equals"
+  }
+}
+
+resource "vercel_project_domain" "production" {
+  project_id = vercel_project.portfolio.id
+  domain     = "mervinhemaraju.com"
+}
+
+# Preview domain - assigned to all other branches (preview deployments)
+resource "vercel_project_domain" "preview" {
+  project_id            = vercel_project.portfolio.id
+  domain                = "staging.mervinhemaraju.com"
+  custom_environment_id = vercel_custom_environment.staging.id
+}
