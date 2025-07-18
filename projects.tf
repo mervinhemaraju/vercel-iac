7c007612fa94cd0738fd96d237011a84604e7c6b
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-07-18 22:51:50 +0400

    - Added a new portfolio project

---
+++ projects.tf
@@ -0,0 +1,10 @@
+# The portfolio project
+resource "vercel_project" "portfolio" {
+  name      = "portfolio"
+  framework = "other"
+
+  git_repository = {
+    type = "github"
+    repo = "mervinhemaraju/portfolio"
+  }
+}
