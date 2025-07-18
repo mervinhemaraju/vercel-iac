b00b36a3cab2dc626710ed42af1ef267e69643fe
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-07-18 23:20:24 +0400

    -

--- projects.tf
+++ projects.tf
@@ -10,24 +10,13 @@ resource "vercel_project" "portfolio" {
 }
 
 
-resource "vercel_custom_environment" "staging" {
-  project_id  = vercel_project.portfolio.id
-  name        = "staging"
-  description = "A description of the custom environment"
-  branch_tracking = {
-    pattern = "staging"
-    type    = "equals"
-  }
-}
-
 resource "vercel_project_domain" "production" {
   project_id = vercel_project.portfolio.id
   domain     = "mervinhemaraju.com"
 }
 
-# Preview domain - assigned to all other branches (preview deployments)
 resource "vercel_project_domain" "preview" {
-  project_id            = vercel_project.portfolio.id
-  domain                = "staging.mervinhemaraju.com"
-  custom_environment_id = vercel_custom_environment.staging.id
+  project_id = vercel_project.portfolio.id
+  domain     = "staging.mervinhemaraju.com"
+  git_branch = "staging"
 }
