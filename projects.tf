f05dd5dcf4a8fde3f50e9b5ae00bfb09bb948206
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-09-06 23:51:07 +0400

    - Fixed issue in domain creation

--- projects.tf
+++ projects.tf
@@ -11,7 +11,7 @@ module "portfolio" {
       domain     = "mervinhemaraju.com"
     },
     {
-      top_domain = "mervinhemaraju.com"
+      top_domain = "@"
       domain     = "mervinhemaraju.com"
     }
   ]
